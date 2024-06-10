98798c3918819e3fa5348ce29c12a8f24455ae4e
Malis_pv <mgenius012975@gmail.com>
2024-06-10 13:07:06 +0300

    Work Edition v1.1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,28 +25,6 @@ public class Main {
         Session session = sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
 
-        //Infinity cycle for input.
-//        Get course Java-developer!
-//        String text = "";
-//        while (!text.equals("Java-разработчик")) {
-//            System.out.println("Input search course: ");
-//            text = new Scanner(System.in).nextLine();
-//            if (text.equals("Java-разработчик")) {
-//
-//                Course course = session.get(Course.class, "Java-разработчик");
-//                System.out.println(course.toString());
-//            }
-//        }
-
-//        Course course = session.get(Course.class, "Java-разработчик");
-//        Teacher teacher =  course.getTeacher();
-//        System.out.println("Information about teacher: " + teacher.toString());
-
-//        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
-//        CriteriaQuery<Subscription> courseCriteriaQuery = criteriaBuilder.createQuery(Subscription.class);
-//        Root<Subscription> root = courseCriteriaQuery.from(Subscription.class);
-//        courseCriteriaQuery.select(root);
-//        List<Subscription> purchasesList = session.createQuery(courseCriteriaQuery).getResultList();
         CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
         CriteriaQuery<PurchaseList> courseCriteriaQuery = criteriaBuilder.createQuery(PurchaseList.class);
         Root<PurchaseList> root = courseCriteriaQuery.from(PurchaseList.class);
@@ -54,18 +32,13 @@ public class Main {
         List<PurchaseList> purchasesList = session.createQuery(courseCriteriaQuery).getResultList();
         try {
             purchasesList.forEach(pL -> {
-//                LinkedPurchaseListId id = new LinkedPurchaseListId(pL.getStudentId(), pL.getCourseId());
-//                LinkedPurchaseList linkedPurchaseList = new LinkedPurchaseList(id);
-//
-//                session.save(linkedPurchaseList);
-//                session.flush();
-//                session.clear();
-//
-//                System.out.println(linkedPurchaseList);
-                PurchaseListId id = new PurchaseListId(pL.getPurchaseListId().getStudentName(), pL.getPurchaseListId().getCourseName());
-                PurchaseList linkedPurchaseList = new PurchaseList(id);
+                Student student = session.get(Student.class, pL.getPurchaseListId().getStudentName());
+                Course course = session.get(Course.class, pL.getPurchaseListId().getCourseName());
+                int studentId = student.getId();
+                int courseId = course.getId();
+                System.out.println("Student id: " + studentId + "\nCourse id: " + courseId);
 
-                System.out.println(linkedPurchaseList);
+                LinkedPurchaseList linkedPurchaseList = new LinkedPurchaseList(new LinkedPurchaseListId(studentId, courseId));
                 session.save(linkedPurchaseList);
                 session.flush();
                 session.clear();
@@ -75,18 +48,6 @@ public class Main {
         } catch (Exception ex) {
             ex.getMessage();
         }
-
-//        LinkedPurchaseList linkedPurchaseList = new LinkedPurchaseList();
-//        Student student = new Student();
-//        student.setName("Vasya");
-//        Course course = new Course();
-//        course.setName("Java and Python");
-//        for (int i = 0; i < 10; i++) {
-//            Course currentCourse = session.get(Course.class, "Java-разработчик");
-//            System.out.println("Course id: " + currentCourse.getId() + "\nCourse name: " + currentCourse.getName());
-//        }
-//        linkedPurchaseList.setStudentId(currentCourse.getId());
-//        linkedPurchaseList.setCourseId(currentCourse.getId());
 
         transaction.commit();
         sessionFactory.close();
